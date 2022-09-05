7f6566d1e5bc197dd5554e2b74120b691d49af22
Javier Almeida <108470987+javieralmeida30@users.noreply.github.com>
2022-09-04 21:21:04 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,12 +1,19 @@
+
 function cargarSubmit(event){
   event.preventDefault();
-  const usuario=document.getElementById("usuarioPrincipal").value;
-  const constrasena= document.getElementById("contrasena").value;
-  if (usuario !='' && constrasena !='') {
+  const contrasena=document.getElementById("contraseña").value;
+  const correo=document.getElementById("usuarioPrincipal").value;
+  if (contrasena != "" && correo != "") {
     window.open("https://javieralmeida30.github.io/comjap.github.io/cover", "_self")
+    localStorage.usuario = correo
   }
-}
-function goog(){
- 
-  window.open("https://javieralmeida30.github.io/comjap.github.io/cover", "_self")
+  else if (contrasena != "") {
+      alert("Campos vacíos:\n e-mail");
+  }
+  else if (correo != ""){
+      alert("Campos vacíos:\n contraseña");
+  }
+  else {
+      alert("Campos vacíos:\n e-mail \ncontraseña")
+  }
 }
