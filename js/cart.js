38601b9ece5d5e671656e3abad78167cde18e950
Javier Almeida <108470987+javieralmeida30@users.noreply.github.com>
2022-10-30 23:37:25 -0300

    function agregar envio, multiplicador e ids

--- js/cart.js
+++ js/cart.js
@@ -1,16 +1,34 @@
-let costos = document.getElementById("Costos")
-inputCosto = document.getElementById("inputCosto")
-function multipli(){
-  if(inputCosto+=2){
-    return costos*2
-  }
+ 
+
+
+function agregarEnvio(){
+  let envio = document.querySelector("input[name=grupo]:checked").value
+    total = subtotal+(subtotal*(envio/100))
+document.getElementById("footersubtotal").innerHTML = subtotal
+document.getElementById("footerenvio").innerHTML = Math.round((subtotal*(envio/100))) 
+document.getElementById("footertotal").innerHTML = total
+
 }
 
 
+
+
+function multipli(productIndice){
+  let inputCantidad = document.getElementById("inputCantidad").value
+  let subtotalhtml = document.getElementById("subtotal")
+  let costo = product.products[productIndice].cost
+  console.log(product.products[productIndice].cost)
+  subtotal = inputCantidad*costo
+  subtotalhtml.innerHTML = subtotal
+ }
+
+let product = []
+let subtotal = 0
+let total = 0
 getJSONData(PRODUCTS_URL2).then(function (resultObj) {
     console.log(resultObj)
     if (resultObj.status == "ok") {
-         let product = resultObj.data;
+          product = resultObj.data;
          let html = ''; 
          console.log(product) 
      
@@ -32,10 +50,10 @@ getJSONData(PRODUCTS_URL2).then(function (resultObj) {
                      </div>
 
                      <div class="col-md-1">
-                       <input class="form-control-price" onclick="multipli()" id="inputCosto" type="number";"></input>
+                       <input class="form-control-price" onblur="multipli(3)" id="inputCantidad" type="number";"></input>
                      </div>ㅤ
 
-                     <div class="col-md-1" id="Costos">
+                     <div class="col-md-1" id="subtotal">
                       <b>${product.products[3].cost}</b>
                      </div>
                    
