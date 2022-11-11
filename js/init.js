3d6f9eb92a214ec260b62fcda1530db382895e80
Javier Almeida <108470987+javieralmeida30@users.noreply.github.com>
2022-11-11 20:14:05 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,10 @@
 const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
 const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
 const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/"+ localStorage.getItem('catID')+".json";
+const PRODUCTS_URL2 = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
 const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
-const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
+const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
 
